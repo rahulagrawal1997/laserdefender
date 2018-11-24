15336eff7926548340e663b5937059e8f60acda6
rahulagrawal1997 <33540082+rahulagrawal1997@users.noreply.github.com>
2018-11-24 22:26:57 +0530

    Laserdefender
    2D game

---
+++ New Unity Project/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+	[SerializeField] List<Transform> Waypoints;
+	[SerializeField] float movspeed=2.0f;
+
+	int WaypointIndex=0;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Waypoints [WaypointIndex].position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mov ();
+		
+	}
+
+	void Mov ()
+	{
+		if (WaypointIndex <= Waypoints.Count - 1) {
+			var TargestLoc = Waypoints [WaypointIndex].transform.position;
+			var movmentspeed = movspeed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards (transform.position, TargestLoc, movmentspeed);
+			if (Vector3.Distance(transform.position,TargestLoc) <= 0.1f) {
+				WaypointIndex++;
+			}
+		}
+		else {
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ New Unity Project/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Enemy Wave Config")]
+public class WaveConfig : ScriptableObject {
+
+	[SerializeField] GameObject enemyPrefab;
+	[SerializeField] GameObject pathPrefab;
+	[SerializeField] float timebetweenspawn=0.5f;
+	[SerializeField] float spawnRandomFactor=0.3f;
+	[SerializeField] int numberOFEnemies=5;
+	[SerializeField] float movSpeed=2f;
+
+	public GameObject GetenemyPrefab(){return enemyPrefab;}
+
+	public GameObject GetpathPrefab(){return pathPrefab;}
+	public float Gettimebetweenspawn(){return timebetweenspawn;}
+	public float GetspawnRandomFactor(){return spawnRandomFactor;}
+	public int GetnumberOFEnemeies(){return numberOFEnemies;}
+	public float GetmovSpeed(){return timebetweenspawn;}
+}
