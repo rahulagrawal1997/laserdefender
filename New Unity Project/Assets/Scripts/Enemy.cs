2a9966e2b1d44e4ef45c426580ddf9f5223b38f1
rahulagrawal1997 <33540082+rahulagrawal1997@users.noreply.github.com>
2018-11-26 14:41:01 +0530

    Enemy Destruction
    Enemy movement and destruction of enemy when its health is 0

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	[SerializeField]  int health=100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Damage damageDealer = other.gameObject.GetComponent<Damage> ();
+		ProcessHit (damageDealer);
+	}
+
+	void ProcessHit (Damage damageDealer)
+	{
+		health -= damageDealer.GetDamage ();
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
