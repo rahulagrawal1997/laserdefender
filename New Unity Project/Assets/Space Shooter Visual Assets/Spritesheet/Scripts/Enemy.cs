7bf8d01b061c7f8afb50af85f9416f9bcac027cf
rahulagrawal1997 <33540082+rahulagrawal1997@users.noreply.github.com>
2018-11-27 21:43:46 +0530

    Enemy shooter
    In this commit enemy can shoot and destroy the player

---
+++ New Unity Project/Assets/Space Shooter Visual Assets/Spritesheet/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	[SerializeField]  int health=100;
+	[SerializeField] float shotCounter;
+	[SerializeField] float minTimeBetweenShots=0.2f;
+	[SerializeField] float maxTimeBetweenShots=3f;
+	[SerializeField] GameObject projectile;
+	[SerializeField] float projectileSpeed=10f;
+	// Use this for initialization
+	void Start () {
+		shotCounter = Random.Range (minTimeBetweenShots, maxTimeBetweenShots);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CountDownAndShoot ();
+	}
+
+	void CountDownAndShoot ()
+	{
+		shotCounter -= Time.deltaTime;
+		if (shotCounter <= 0f) {
+			Fire();
+		}
+	}
+
+	private void Fire(){
+		GameObject laser = Instantiate (
+			projectile,
+			transform.position,
+			Quaternion.identity
+		)as GameObject;
+		laser.GetComponent<Rigidbody2D>().velocity=new Vector2(0, -projectileSpeed);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Damage damageDealer = other.gameObject.GetComponent<Damage> ();
+		ProcessHit (damageDealer);
+	}
+
+	void ProcessHit (Damage damageDealer)
+	{
+		health -= damageDealer.GetDamage ();
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ New Unity Project/Assets/Space Shooter Visual Assets/Spritesheet/Scripts/player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	[SerializeField] float MovSpeed=10f;
+	[SerializeField] float padding=1f;
+	[SerializeField] GameObject Laserprefab;
+	[SerializeField] float projectileSpeed=10f;
+	[SerializeField] float projectilefireperid=0.1f;
+	Coroutine firingCoroutine;
+	float xMin;
+	float xMax;
+	float yMin;
+	float yMax;
+
+	// Use this for initialization
+	void Start () {
+		SetUpMovBoundaries ();
+	}
+
+	private void SetUpMovBoundaries ()
+	{
+		Camera GameCamera=Camera.main;
+		xMin = GameCamera.ViewportToWorldPoint (new Vector3 (0, 0, 0)).x+padding;
+		xMax = GameCamera.ViewportToWorldPoint (new Vector3 (1, 0, 0)).x-padding;
+		yMin = GameCamera.ViewportToWorldPoint (new Vector3 (0, 0, 0)).y+padding;
+		yMax = GameCamera.ViewportToWorldPoint (new Vector3 (0, 1, 0)).y-padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+		Fire ();
+		
+	}
+
+	void Fire ()
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			firingCoroutine= StartCoroutine(FireCountinously());
+		
+		}
+		if (Input.GetButtonUp ("Fire1")) {
+			StopCoroutine(firingCoroutine);
+		}
+	}
+	IEnumerator FireCountinously()
+	{
+		while (true) {
+			GameObject laser= Instantiate(Laserprefab,transform.position,Quaternion.identity) as GameObject;
+			laser.GetComponent<Rigidbody2D>().velocity=new Vector2(0, projectileSpeed);
+			yield return new WaitForSeconds(projectilefireperid);
+		}
+	}
+
+	private void Move(){
+		var DeltaX = Input.GetAxis ("Horizontal") * Time.deltaTime*MovSpeed;
+		var DeltaY = Input.GetAxis ("Vertical") * Time.deltaTime*MovSpeed;
+
+		var NewXPos = Mathf.Clamp(transform.position.x + DeltaX,xMin,xMax);
+		var NewYPos = Mathf.Clamp(transform.position.y + DeltaY, xMin,xMax);
+
+		transform.position = new Vector2 (NewXPos,NewYPos);
+	}
+}
